64e5f51f144f626c836c4d3097ba9d1cf760e5d1
Florian Thiel <flth@flth.de>
2023-01-03 21:40:20 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,30 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "modwerkstatt", "deutschland", "germany", "db", "waggon", "goods", "basis", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+        params = {
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,16 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "ModWerkstatt Base Mod",
+		["mod_desc"] = "The base mod is needed for many mods of the ModWerkstatt. It currently contains some bogies that are a revision of the \"kaleut_drehgestelle\" mod. But this mod is independent of it.\nMore features will follow.",
+
+	},
+	de = {
+
+		["mod_name"] = "ModWerkstatt Basis Mod",
+		["mod_desc"] = "Der Basismod wird benötigt für viele Mods der ModWerkstatt. Er enthält momentan einige Drehgestelle, die eine Überarbeitung des \"kaleut_drehgestelle\" Mods sind. Dieser Mod ist aber unabhängig davon.\nWeitere Funktionen werden folgen.",
+
+	},
+}
+end
