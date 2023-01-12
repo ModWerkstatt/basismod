f151bfe6cdf05e7de93a53a7fa2581aad1c22357
Florian Thiel <flth@flth.de>
2023-01-13 00:02:27 +0100

    Beschreibung von Steam übernommen

--- strings.lua
+++ strings.lua
@@ -3,13 +3,13 @@ return {
 	en = {
 
 		["mod_name"] = "ModWerkstatt Base Mod",
-		["mod_desc"] = "The base mod is needed for many mods of the ModWerkstatt. It currently contains some bogies that are a revision of the \"kaleut_drehgestelle\" mod. But this mod is independent of it.\nMore features will follow.",
+		["mod_desc"] = "The base mod is needed for many mods of the ModWerkstatt.\n\nCurrently it contains some bogies that are a revision of the “kaleut_drehgestelle” mod. The base mod does not replace the bogies mod, as it is still needed by many mods.\nThis mod is independent of it and offers new features and a better quality than before. We will equip all wagons with the base mod in the future.\nSimilarly, the train end is also different from the train end mod. But all mods that need the base mod do not need the train end mod.\n\nMore functions will follow.",
 
 	},
 	de = {
 
 		["mod_name"] = "ModWerkstatt Basis Mod",
-		["mod_desc"] = "Der Basismod wird benötigt für viele Mods der ModWerkstatt. Er enthält momentan einige Drehgestelle, die eine Überarbeitung des \"kaleut_drehgestelle\" Mods sind. Dieser Mod ist aber unabhängig davon.\nWeitere Funktionen werden folgen.",
+		["mod_desc"] = "Der Basismod wird benötigt für viele Mods der ModWerkstatt.\n\nDerzeit enthält er einige Drehgestelle, die eine Überarbeitung des “kaleut_drehgestelle” Mods sind. Der Basismod ersetzt den Mod der Drehgestelle aber nicht, da dieser weiterhin von vielen Mods benötigt wird.\nDieser Mod ist unabhängig davon und bietet neue Features sowie eine bessere Qualiät als bisher. Wir werden zukünftig alle Wagen mit dem Basismod ausrüsten.\nÄhnlich verhält es sich mit dem Zugschluß, auch dieser ist anders als im Zugschlußmod. Aber alle Mods, die den Basismod benötigen, benötigen den Zugschlußmod nicht.\n\nWeitere Funktionen werden folgen.",
 
 	},
 }
