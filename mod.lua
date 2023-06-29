e3eaec64a1ee83f3334c29df031a6fd89ac0fb20
Florian Thiel <flth@flth.de>
2023-06-29 19:58:58 +0200

    neues Drehgestell hinzugefügt, zusätzliche Textur

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 ﻿function data()
 return {
 	info = {
-		minorVersion = 1,
+		minorVersion = 0,
 		severityAdd = "NONE",
 		severityRemove = "WARNING",
 		name = _("mod_name"),
@@ -22,9 +22,9 @@ return {
 	},
 	options = {
 	},
-
+	
 	runFn = function (settings, modParams)
-
+	
 	end
 	}
 end
